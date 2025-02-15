196cee69850ba5d58bf9a876879b582d2fdb7755
niandra-l <185798403+niandra-l@users.noreply.github.com>
2025-02-15 12:04:51 +0100

    Add columns to the makeGrid function, change grid-changecontainer class name to change-grid

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const container = document.querySelector(".container");
 function makeGrid(x) {
     for (i=0; i<x; i++) {
         let row = document.createElement("div");
-        container.appendChild(row);
+        container.appendChild(row).className="gridRow";
+        for (let j=0; j<x; j++) {
+            col = document.createElement("div");
+            row.appendChild(col).className="gridCol";  
+        }
     }
+
 }
 
 
